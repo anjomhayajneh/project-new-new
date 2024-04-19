2223fae5ba9b5cefdd0bc94cab520e4c90123586
Anjom Hayajneh <144284233+anjomhayajneh@users.noreply.github.com>
2024-04-19 07:21:56 +0300

    18-0 4-2024

---
+++ resources/views/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="app.css">
+</head>
+<body>
+    <div class="card">
+    <div class="card-header">
+        <h1 class="text-center">login</h1>
+</div>
+<div class="card-body">
+    <form action=""method="post">
+    <div class="row">
+<label for="" class="label-text">user name</label>
+<input type="text" name="Name">
+    </div>
+    <div class="row">
+<label for="" class="label-text">password</label>
+<input type="password" name="Name">
+    </div>
+    <div class="row">
+        <a href="" class="btn btn-success">login</a>
+        <a href="" class="btn btn-danger">cancel</a>
+     
+
+    </div>
+</form>
+</div>
+</div>
+</body>
+</html>
